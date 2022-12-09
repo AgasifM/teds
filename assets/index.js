3d736d3ed22060f5f586e48378536455dd984aad
AgasifM <agasifmahmudov516@gmail.com>
2022-12-09 18:14:24 +0400

    first commit

---
+++ assets/index.js
@@ -0,0 +1,40 @@
+var x = prompt(" aylari reqemle tap");
+switch (x) {
+  
+  case "2"||"12":
+    alert("bu Qis Ayidir");
+    break;
+  case "3":
+    alert("Bu Y az Ayidir");
+    break;
+  case "4":
+    alert("Bu Y az Ayidir");
+    break;
+  case "5":
+    alert("Bu Yaz Ayidir");
+    break;
+  case "6":
+    alert(" Bu yay ayidir")
+    break;
+  case "7":
+    alert("Bu yay ayidir");
+    break;
+  case "8":
+    alert("Bu yay ayidir");
+    break;
+  case "9":
+    alert("bu payiz ayidir");
+    break;
+  case "10":
+    alert("bu payiz ayidir");
+    break;
+  case "11":
+    alert("bu payiz ayidir");
+    break;
+  case "12":
+    alert("BU qis Ayidir");
+    break;
+ 
+  default:
+    alert(" bele ay yoxdur");
+}
